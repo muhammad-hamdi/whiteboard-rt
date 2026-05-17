c3af78afb3abef09836df20882506ef1a70b3af2
codelink7 <codelink7@gmail.com>
2026-05-17 15:52:10 +0300

    Update main.ts

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -55,11 +55,6 @@ let wbCanvas: Canvas = {
 
 let cursors: Map<string, {p:Vec2, c:string}> = new Map()
 
-const randRange = (min: number = 0, max: number = 1) => {
-    let r = min/max
-    return (Math.random()+r) * max
-}
-
 const randColor = () => {
         let r: number, g: number, b: number
     do {
